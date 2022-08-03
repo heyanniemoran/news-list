b42a26d15ddc8a47cb59e507dc80597733ebedff
heyanniemoran <heyanniemoran@gmail.com>
2022-08-03 23:21:24 +0300

    feat: причесала страницу со списком новостей

---
+++ src/components/Home.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Home() {
+  return (
+    <Container>
+      <div className="py-2">
+        <Button variant="primary" size="sm">
+          Reload news list
+        </Button>
+      </div>
+      <ListGroup numbered>
+        <ListGroup.Item
+          action
+          href="#"
+          className="d-flex justify-content-start align-items-start"
+        >
+          <div className="d-flex flex-column ms-3">
+            <h5>News Title</h5>
+            <div className="d-flex justify-content-start align-items-start">
+              <span className="me-3">Rating</span>
+              <span className="me-3">Author</span>
+              <span>Date</span>
+            </div>
+          </div>
+        </ListGroup.Item>
+      </ListGroup>
+    </Container>
+  );
+}
+
+export default Home;
