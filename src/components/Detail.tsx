7ecc71dde0427f95f7f1aa142e52e539ff6433f1
heyanniemoran <heyanniemoran@gmail.com>
2022-08-03 23:16:49 +0300

    feat: заверстала детальную страницу новости

---
+++ src/components/Detail.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Badge from "react-bootstrap/Badge";
+import Stack from "react-bootstrap/Stack";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Detail() {
+  return (
+    <Container>
+      <h1>Title</h1>
+      <div className="d-flex align-items-baseline justify-content-start">
+        <span className="me-3">Date</span>
+        <span className="me-3">Author</span>
+        <a href="#" className="me-3">
+          Link
+        </a>
+        <Badge pill bg="info">
+          12
+        </Badge>
+      </div>
+      <Stack gap={2} className="mt-3">
+        <div className="border p-2">
+          <Button variant="primary" size="sm">
+            Обновить
+          </Button>
+          <Button variant="link" size="sm">
+            Вернуться к списку
+          </Button>
+        </div>
+        <div className="border p-2">Comments</div>
+      </Stack>
+    </Container>
+  );
+}
+
+export default Detail;
